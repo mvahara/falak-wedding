97f4e27cfcc76e2bc88b3f1be868c35121005cd8
mvahara <mvahara786@gmail.com>
2026-09-25 22:24:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,3 +95,14 @@ sections.forEach((section) => {
     observer.observe(section);
 
 });
+function checkPassword() {
+    const correctPassword = "Mohammad";
+    const enteredPassword = document.getElementById("passwordInput").value;
+    const errorMessage = document.getElementById("passwordError");
+
+    if (enteredPassword === correctPassword) {
+        document.getElementById("passwordScreen").style.display = "none";
+    } else {
+        errorMessage.textContent = "Incorrect password. Please try again.";
+    }
+}
